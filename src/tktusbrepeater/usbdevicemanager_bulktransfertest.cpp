46a9ac4976a072c4b122bc86efadef3e69b5cb39
tekuto <g.tekuto@gmail.com>
2023-05-07 17:20:24 +0900

    UsbDeviceManager::bulkTransfer()のテストを追加

--- src/tktusbrepeater/usbdevicemanager_bulktransfertest.cpp
+++ src/tktusbrepeater/usbdevicemanager_bulktransfertest.cpp
@@ -14,25 +14,29 @@ namespace {
         }
 
     public:
+        UsbDeviceHandleImpl *   deviceHandleImpl = reinterpret_cast< UsbDeviceHandleImpl * >( 10 );
+
+        const int   TRANSFERRED = 20;
+
         void test(
+            UsbDeviceHandleImpl *   _deviceHandleImpl
+            , int                   _EXPECTED
+            , int                   _EXPECTED_CALLED_COUNT_BULK_TRANSFER_USB_IMPL
+            , UsbDeviceHandleImpl * _EXPECTED_ARG_BULK_TRANSFER_USB_IMPL_DEVICE_HANDLE_IMPL
         ) const
         {
-            auto    deviceHandleImpl = reinterpret_cast< UsbDeviceHandleImpl * >( 10 );
-
-            const auto  ENDPOINT = 20;
-            const auto  DATA = reinterpret_cast< void * >( 30 );
-            const auto  DATA_SIZE = 40;
-
-            const auto  TRANSFERRED = 50;
+            const auto  ENDPOINT = 30;
+            const auto  DATA = reinterpret_cast< void * >( 40 );
+            const auto  DATA_SIZE = 50;
 
             auto    usbDeviceManagerImpl = UsbDeviceManagerImpl{
-                .usbDeviceHandleUnique = UsbDeviceHandleImplUnique( deviceHandleImpl ),
+                .usbDeviceHandleUnique = UsbDeviceHandleImplUnique( _deviceHandleImpl ),
             };
 
-            returnsBulkTransferUsbImpl = TRANSFERRED;
+            returnsBulkTransferUsbImpl = this->TRANSFERRED;
 
             EXPECT_EQ(
-                TRANSFERRED
+                _EXPECTED
                 , reinterpret_cast< UsbDeviceManager & >( usbDeviceManagerImpl ).bulkTransfer(
                     ENDPOINT
                     , DATA
@@ -40,11 +44,13 @@ namespace {
                 )
             );
 
-            EXPECT_EQ( 1, calledCountBulkTransferUsbImpl );
-            EXPECT_EQ( deviceHandleImpl, argsBulkTransferUsbImpl.deviceHandle );
-            EXPECT_EQ( ENDPOINT, argsBulkTransferUsbImpl.endpoint );
-            EXPECT_EQ( DATA, argsBulkTransferUsbImpl.data );
-            EXPECT_EQ( DATA_SIZE, argsBulkTransferUsbImpl.dataSize );
+            EXPECT_EQ( _EXPECTED_CALLED_COUNT_BULK_TRANSFER_USB_IMPL, calledCountBulkTransferUsbImpl );
+            if( calledCountBulkTransferUsbImpl > 0 ) {
+                EXPECT_EQ( _EXPECTED_ARG_BULK_TRANSFER_USB_IMPL_DEVICE_HANDLE_IMPL, argsBulkTransferUsbImpl.deviceHandle );
+                EXPECT_EQ( ENDPOINT, argsBulkTransferUsbImpl.endpoint );
+                EXPECT_EQ( DATA, argsBulkTransferUsbImpl.data );
+                EXPECT_EQ( DATA_SIZE, argsBulkTransferUsbImpl.dataSize );
+            }
         }
     };
 }
@@ -54,5 +60,23 @@ TEST_F(
     , BulkTransfer
 )
 {
-    this->test();
+    this->test(
+        this->deviceHandleImpl
+        , this->TRANSFERRED
+        , 1
+        , this->deviceHandleImpl
+    );
+}
+
+TEST_F(
+    UsbDeviceManager_bulkTransferTest
+    , NotConnected
+)
+{
+    this->test(
+        nullptr
+        , 0
+        , 0
+        , nullptr
+    );
 }
