3a445be83178b0d5f154f2e7e6249dc89c053528
tekuto <g.tekuto@gmail.com>
2023-05-21 15:25:32 +0900

    initializeCommandLineOptions()の処理を追加

--- src/tktusbrepeater/commandlineoptions.cpp
+++ src/tktusbrepeater/commandlineoptions.cpp
@@ -1,4 +1,26 @@
 #include "tktusbrepeater/commandlineoptions.h"
+#include <unistd.h>
+#include <string>
+
+namespace {
+    enum {
+        OPTION_KEY_SOCKET_NAME = 's',
+        OPTION_KEY_VENDOR_ID = 'v',
+        OPTION_KEY_PRODUCT_ID = 'p',
+        OPTION_KEY_HELP = 'h',
+    };
+
+    int hexStringToInt(
+        const char *    _STRING
+    )
+    {
+        return std::stoi(
+            _STRING
+            , nullptr
+            , 16
+        );
+    }
+}
 
 bool initializeCommandLineOptions(
     CommandLineOptions &    _commandLineOptions
@@ -6,6 +28,45 @@ bool initializeCommandLineOptions(
     , char * const *        _argv
 )
 {
-    //TODO
-    return false;
+    while( true ) {
+        const auto  OPTION_KEY = getopt(
+            _ARGC
+            , _argv
+            , "s:v:p:h"
+        );
+        if( OPTION_KEY < 0 ) {
+            break;
+        }
+
+        switch( OPTION_KEY ) {
+        case OPTION_KEY_SOCKET_NAME:
+            _commandLineOptions.socketName = std::string( optarg );
+            break;
+
+        case OPTION_KEY_VENDOR_ID:
+            _commandLineOptions.vendorId = hexStringToInt( optarg );
+            break;
+
+        case OPTION_KEY_PRODUCT_ID:
+            _commandLineOptions.productId = hexStringToInt( optarg );
+            break;
+
+/*
+        case OPTION_KEY_HELP:
+            printHelp = true;
+            goto LOOP_END;
+            break;
+*/
+
+        default:
+/*
+            printHelp = true;
+*/
+            goto LOOP_END;
+            break;
+        }
+    }
+    LOOP_END:
+
+    return true;
 }
